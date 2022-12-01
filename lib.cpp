6990456f929747a9b66670452da01d9d4d252f59
EdoardoMaestri <115471996+EdoardoMaestri@users.noreply.github.com>
2022-12-01 10:15:42 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
 
+#include "lib.h"
+
+bool trovaprimo (int dato, int passo)
+{
+    bool flag=false;
+
+    if (passo == 1 || passo == 0)
+    {
+        return true;
+    }
+
+    if ((dato % passo) != 0)
+    {
+        passo--;
+
+        flag = trovaprimo(dato, passo);
+
+        return flag;
+    }
+}
+
